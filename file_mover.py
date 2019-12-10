669b5b616b93f6eb6c3b29180030428e144ccd02
pkhetland <petter.hetland@gmail.com>
2019-12-11 00:21:26 +0100

    Added complexity to script.

--- file_mover.py
+++ file_mover.py
@@ -1,32 +1,52 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-
 import os
 import json
 import time
+import shutil
+
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        for filename in os.listdir(folder_to_track):
-            if "1219" in filename.lower() or "servicekort" in filename.lower():
-                src = folder_to_track + "/" + filename
-                new_destination = folder_destination + "/" + filename
+        for filename in os.listdir(meditek_folder):
+            if (
+                    "fraktdokumenter" in filename.lower()
+                    or "export" in filename.lower()
+                    or "servicekort" in filename.lower()
+                    or "fraktdokument" in filename.lower()
+                    or "shipping" in filename.lower()
+                    and filename != 'Servicedokumenter'
+            ):
+                src = meditek_folder + "/" + filename
+                new_destination = folder_destination + "/Servicedokumenter/" + filename.replace(" ", "_")
+                os.rename(src, new_destination)
+
+            elif "calc" in filename.lower() or "tilbud" in filename.lower() and filename != 'Tilbud':
+                src = meditek_folder + "/" + filename
+                new_destination = folder_destination + "/Tilbud/" + filename.replace(" ", "_")
+                os.rename(src, new_destination)
+
+        for filename in os.listdir(servicedokumenter_folder):
+            if "1219" in filename:
+                src = servicedokumenter_folder + "/" + filename
+                new_destination = servicedokumenter_folder + "/desember/" + filename.replace(" ", "_")
                 os.rename(src, new_destination)
 
 
-folder_to_track = "/users/petter/google-drive/Jobb/Meditek"
-folder_destination = "/users/petter/google-drive/Jobb/Meditek/A-Servicedokumenter-Desember"
+meditek_folder = "/Users/petter/google-drive/Jobb/Meditek"
+servicedokumenter_folder = "/Users/petter/google-drive/Jobb/Meditek/Servicedokumenter"
+folder_destination = "/Users/petter/google-drive/Jobb/Meditek"
 
 event_handler = MyHandler()
 
 observer = Observer()
-observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.schedule(event_handler, meditek_folder, recursive=True)
 observer.start()
 
 try:
     while True:
         time.sleep(10)
-except(KeyboardInterrupt):
+except KeyboardInterrupt:
     observer.stop()
 observer.join()
